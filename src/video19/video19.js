e7a0cfef70cda8ee9dc2fba9e75a70bc2a59d677
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 19:31:49 +0700

    Object Data Type: Object and Array

---
+++ src/video19/video19.js
@@ -0,0 +1,15 @@
+console.log("video 19");
+
+//object: key-value
+const person = {
+    name: "Duy",
+    age: 26,
+    address: "hcm"
+}
+
+console.log(person);
+
+//array
+const score = [5, 6, 7, 8];
+
+console.log(score);
