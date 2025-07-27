75a171303b08477ebc054a76b685221082b2980a
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 20:32:44 +0700

    Template String

---
+++ src/video22/video22.js
@@ -0,0 +1,11 @@
+console.log("video 22");
+
+const name = "Duy";
+const age = 26;
+
+const introduction = "my name is " + name + " and \n I'm " + age;
+console.log(introduction);
+
+const intro = `my name is "test" 'test1' ${name} and 
+I'm ${age}`;
+console.log(intro);
