8520d067d76a55910a7146878552aec55b670d21
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 19:22:42 +0700

    Primitive Data type

---
+++ src/video18/video18.js
@@ -0,0 +1,25 @@
+
+//number
+const myAge = 20;
+const myScore = 9.1;
+
+console.log("video 18");
+
+//string 
+const myName = "Duy"; //java
+const name = 'duy';
+const name1 = `DD`;
+
+console.log(myName);
+
+//boolean
+const isDeptrai = false;
+console.log(isDeptrai);
+
+//undefined
+let bull;
+console.log(bull);
+
+//null
+let testNull = null;
+console.log(testNull);
