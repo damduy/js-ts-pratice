b26d3ad92ca682535fe5137f38df584d7874c4c9
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 19:03:11 +0700

    Learn about const, let and var

---
+++ src/video17/video17.js
@@ -0,0 +1,17 @@
+console.log("Video 17");
+
+var name = 'duy';
+
+const name1 = "damduy";
+
+let name2 = 'hello';
+
+const age = 20;
+
+let myAge = 25;
+
+myAge = 30;
+
+console.log(name);
+
+console.log(myAge);
