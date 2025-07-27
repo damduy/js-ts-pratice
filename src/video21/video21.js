10ae52eec91e52cce1b7e9e122288bf162f67bfe
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 20:15:47 +0700

    Type of Console command

---
+++ src/video21/video21.js
@@ -0,0 +1,18 @@
+console.log("video 21");
+
+
+console.log("info");
+console.error("I make a mistake");
+console.warn("test warning");
+/*
+Many lines comment
+*/
+const name = "Duy";
+const age = 26;
+
+console.log(name, age);
+
+console.log("Name: " + name + ", Age: " + age);
+console.log("Name:",name,",Age:",age);
+
+console.log("%cWarning!","color:red; font-weight:bold;");
