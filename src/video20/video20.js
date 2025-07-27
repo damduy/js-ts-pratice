a8ffed4ae30907358569447bf49d981ac73cd828
damduy <80811184+damduy@users.noreply.github.com>
2025-07-27 19:54:51 +0700

    Different type of operator

---
+++ src/video20/video20.js
@@ -0,0 +1,24 @@
+console.log("video 20");
+
+// const a = 10;
+// const b = 20, c=5;
+
+// console.log(a+b);
+
+// const d = a+b*c;
+// console.log(d);
+
+// console.log(b%c);
+
+// let a = 5;
+// const b = ++a; //instant increase
+// const b = a++;
+
+// console.log("a = " + a);
+// console.log("b = " + b);
+
+const a = 5; //number
+const b = "5"; //string
+
+console.log(a==b);
+console.log(a===b);
