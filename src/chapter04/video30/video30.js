9faf5e24e28a77da578c3fee94bab357a7eabca4
damduy <80811184+damduy@users.noreply.github.com>
2025-07-28 15:04:16 +0700

    Function

---
+++ src/chapter04/video30/video30.js
@@ -0,0 +1,17 @@
+console.log("video 30");
+
+function greeting() {
+    console.log("Hello");
+}
+
+greeting();
+
+//sum a+b
+
+function sum(a, b) { //param
+    return a + b;
+}
+
+console.log(sum(10, 5)); //input
+
+console.log(sum("Hello ", "Everyone"));
