6ebe0c6a4c21b67a051b9789f612af6ca058b623
damduy <80811184+damduy@users.noreply.github.com>
2025-07-28 15:14:27 +0700

    Arrow function

---
+++ src/chapter04/video31/video31.js
@@ -0,0 +1,17 @@
+console.log("video 31");
+
+// function sum(a, b) {
+//     return a+b;
+// }
+
+const sum = (a, b) => {
+    return a+b;
+}
+
+
+
+console.log(sum(8, 9));
+
+// (function () {
+//     console.log("Hello run");
+// })();
