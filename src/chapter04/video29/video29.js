63e4ae17b71ad0879e82e276de541183e3ed5357
damduy <80811184+damduy@users.noreply.github.com>
2025-07-28 14:53:18 +0700

    Break and continue

---
+++ src/chapter04/video29/video29.js
@@ -0,0 +1,17 @@
+console.log("video 29");
+
+// for (let i = 1; i < 10; i++) {
+//     console.log("i = ", i);
+//     if (i === 5) {
+//         break;
+//     } // 5 vs '5' , number and string
+// }
+
+for (let i = 1; i < 10; i++) {
+    
+    if (i === 5) {
+        continue; // exit current loop
+    } // 5 vs '5' , number and string
+
+    console.log("i = ", i);
+}
