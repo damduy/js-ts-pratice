08d25bb4dbb85bb2b29f40853b362a965272fd73
damduy <80811184+damduy@users.noreply.github.com>
2025-07-29 15:48:12 +0700

    Alert

---
+++ src/chapter06/video54/video54.js
@@ -0,0 +1,9 @@
+console.log("Video 54")
+
+const element = document.getElementById("login");
+
+
+element.addEventListener('click', () => {
+    console.log("Click!");
+    alert("Invalid login");
+})
