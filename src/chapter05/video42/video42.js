fe24397cdf4ab3d3dabad7d7156bc86a5c568245
damduy <80811184+damduy@users.noreply.github.com>
2025-07-29 10:51:17 +0700

    Object

---
+++ src/chapter05/video42/video42.js
@@ -0,0 +1,28 @@
+console.log("Video 42");
+
+const scores = [10, 4, 8, 6];
+
+const sv1 = {
+    score: 10,
+    name: "dd",
+    address: {
+        city: "HCM",
+        country: "VietNam"
+    }
+}
+
+const sv2 = {
+    score: 5,
+    name: "hello",
+    address: {
+        city: "HCM",
+        country: "VietNam"
+    }
+}
+
+const sinhvien = [sv1, sv2];
+
+console.log(">>> Scores: ", scores);
+console.log(">>> Sinh Vien: ", sinhvien);
+
+
