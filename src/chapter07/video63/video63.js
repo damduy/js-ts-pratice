37943aa7cf6ab774393202c2dcf67e5fdd552eaf
damduy <80811184+damduy@users.noreply.github.com>
2025-07-30 10:10:32 +0700

    Call API with fetch

---
+++ src/chapter07/video63/video63.js
@@ -0,0 +1,10 @@
+console.log("Video 63")
+
+//fetch => raw data (Response) => object
+
+const temp = fetch("http://localhost:8000/users")
+
+temp
+    .then(res => res.json() )
+    .then(data => console.log(data))
+
